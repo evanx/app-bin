5e858639fec59de7d5bb7b659eeaa7eed7f898d1
Evan Summers <evan.summers@gmail.com>
2017-02-27 19:35:57 +0000

    publish

--- bin/docker-redeploy.sh
+++ bin/docker-redeploy.sh
@@ -5,6 +5,7 @@ repo=`cat package.json | jq '.repository.url' | sed 's/"\(.*\)"/\1/'`
 echo $name $repo
 
 docker build -t $name $repo
+docker ps -a -q -f "name=/${name}" | xargs -r -n 1 docker rm -f 
 docker rm -f `docker ps -q -f name=$name`
 docker run --name $name -d \
   --network=host \
