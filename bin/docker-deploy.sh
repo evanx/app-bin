ef83304d7417589d5c413560b2f4177f67d175e1
Evan Summers <evan.summers@gmail.com>
2017-03-01 13:01:09 +0000

    publish

--- bin/docker-deploy.sh
+++ bin/docker-deploy.sh
@@ -1,6 +1,8 @@
 set -u -e -x
 name=`basename $PWD`
+timestamp=`stat -c %Z package.json`
 git pull
+[ $timestamp -ne `stat -c %Z package.json` ] && npm install
 docker build -t $name .
 docker rm -f /$name || echo 'not removed'
 docker run --name $name -d \
