c06c5a26bfba71d03b5c14f2a5808a62bec34058
Evan Summers <evan.summers@gmail.com>
2017-03-04 19:45:06 +0000

    publish

--- bin/docker-deploy.sh
+++ bin/docker-deploy.sh
@@ -1,7 +1,16 @@
 set -u -e -x
 name=`basename $PWD`
 timestamp=`stat -c %Z package.json`
+lastCommit() {
+  git log | head -1 | cut -f2 -d' '
+}
+lastCommit=`lastCommit`
 git pull
+if [ "$lastCommit" = `lastCommit` ]
+then
+  sleep 2
+  git pull
+fi
 [ $timestamp -ne `stat -c %Z package.json` ] && npm install
 docker build -t $name .
 docker rm -f /$name || echo 'not removed'
