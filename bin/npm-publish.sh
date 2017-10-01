b824fec34464d9af97631849038265261f0a78fd
Evan Summers <evan.summers@gmail.com>
2017-10-01 10:22:05 +0100

    npm-publish

--- bin/npm-publish.sh
+++ bin/npm-publish.sh
@@ -1,8 +1,6 @@
 
 set -u -e 
 
-message="${1}"
-
   version=`npm version | head -1 |
      sed "s/.*'\([0-9].*\)',/\1/"`
   publishedVersion=`npm info | grep latest | 
@@ -12,8 +10,12 @@ set -x
 
   [ "$version" != "$publishedVersion" ]
 
-  git add -A
-  git commit -m "$message" || echo 'commit failed'
+  if git status | head -2 | tail -1 | grep -qv '^Your branch is up-to-date'
+  then
+    git add -A
+    message="${1}"
+    git commit -m "$message" || echo 'commit failed'
+  fi
   git push
 
   npm publish
